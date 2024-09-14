95a62f96ab09ad956bc0bd2b6481bb53e861fd53
priyayadav1717 <96625946+priyayadav1717@users.noreply.github.com>
2024-09-14 21:49:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const blogRoutes = require('./routes/blog');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Set EJS as the templating engine
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+// Define routes
+app.use(blogRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
